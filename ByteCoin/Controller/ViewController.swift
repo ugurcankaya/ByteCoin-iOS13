d78b0ddb7f44c34592b1a783187a84c7a4fa93fb
Ugurcan Kaya <ugurcankaa@icloud.com>
2020-04-23 23:13:44 +0300

    1.0

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var coinManager = CoinManager()
+    @IBOutlet var picker: UIPickerView!
+    @IBOutlet var valueLabel: UILabel!
+    @IBOutlet var currencySignLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.dataSource = self
         // Do any additional setup after loading the view.
+        picker.delegate = self
+        coinManager.delegate = self
     }
 
 
 }
+extension ViewController:UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return coinManager.currencyArray.count
+    }
+    
+    
+}
 
+extension ViewController:UIPickerViewDelegate{
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        coinManager.getCoinPrice(coinManager.currencyArray[row])
+    }
+}
+
+extension ViewController:CoinManagerDelegate{
+    func didUpdatePrice(price: String, currency: String) {
+        DispatchQueue.main.async {
+             let x = Float(price)
+            print(x!)
+            self.valueLabel.text = String(format: "%.2f", x!)
+            self.currencySignLabel.text = currency
+        }
+        
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+    
+    
+}
---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by UGURCAN KAYA on 4/23/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData:Codable {
+    let rate:Float?
+    let asset_id_quote:String
+}
